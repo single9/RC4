36e68403b4c186afaf9cba8be972135ac2d53a8f
Duye Chen <k078264@gmail.com>
2015-04-08 13:03:41 +0800

    Initial Commit

---
+++ rc4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rc4
+//
+//  Created by Jing Chen on 4/8/15.
+//  Copyright (c) 2015 Single9. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
